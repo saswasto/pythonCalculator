bd201941bce9d0d5ee8405159b7c3b4e405b935e
Sasoto Basak <89937629+saswasto@users.noreply.github.com>
2023-08-14 09:11:14 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,40 @@
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y == 0:
+        return "Cannot divide by zero!"
+    return x / y
+
+def calculator():
+    print("Select operation:")
+    print("1. Addition")
+    print("2. Subtraction")
+    print("3. Multiplication")
+    print("4. Division")
+
+    choice = input("Enter choice (1/2/3/4): ")
+
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+
+    if choice == '1':
+        result = add(num1, num2)
+    elif choice == '2':
+        result = subtract(num1, num2)
+    elif choice == '3':
+        result = multiply(num1, num2)
+    elif choice == '4':
+        result = divide(num1, num2)
+    else:
+        result = "Invalid input"
+
+    print("Result:", result)
+
+calculator()
